fe75fe320f93223af7120999ec648a4c3a9ea81e
yuejindong <89678004+yuejindong@users.noreply.github.com>
2026-01-10 10:44:21 +0800

    Update diy-part1.sh

--- X86/immortalwrt/diy-part1.sh
+++ X86/immortalwrt/diy-part1.sh
@@ -15,3 +15,4 @@
 
 # Add a feed source
 echo 'src-git nikki https://github.com/nikkinikki-org/OpenWrt-nikki.git' >>feeds.conf.default
+sed -i "s/^OPENWRT_RELEASE=.*/OPENWRT_RELEASE='immortalwrt-$(date +%Y%m%d)'/" package/base-files/files/usr/lib/os-release
